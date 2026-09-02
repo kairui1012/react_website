7c8a70cb7291a806103f54a1a4ea41872ab4de13
SAM KAI RUI <samkairui@SAMs-MacBook-Air.local>
2026-09-02 16:56:46 +0800

    last modify at 2/9

--- src/component/elements/Footer.tsx
+++ src/component/elements/Footer.tsx
@@ -5,7 +5,7 @@ import { NavItem } from "../shared/NavItem";
 
 export const Footer = () => {
   return (
-    <footer className="relative pt-28 rounded-t-3xl bg-box-bg border-t border-box-border">
+    <footer className="relative pt-15 rounded-t-3xl bg-box-bg border-t border-box-border">
       <Container className="pb-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex items-center gap-3 mb-4 md:mb-0 ">
